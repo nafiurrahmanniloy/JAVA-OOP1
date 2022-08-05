cce27699fd5fc889218961d4c63c8b1655f9a1ba
nafibhaiya <104713080+nafibhaiya@users.noreply.github.com>
2022-08-05 17:49:27 +0600

    Add files via upload

---
+++ SavingsAccount.java
@@ -0,0 +1,32 @@
+import java.lang.*;
+
+public class SavingsAccount extends Account
+{
+
+	private double interestRate;
+
+
+	public SavingsAccount(int AccountNo,String name,double balance,double interestRate){
+		super(AccountNo,name,balance);
+		
+		this.interestRate=interestRate;
+	}
+	
+	public void setInterestRate(double interestRate)
+	{
+		this.interestRate = interestRate;
+	}
+	public double getInterestRate()
+	{
+		return interestRate;
+	}
+
+	
+
+
+	public void showDetails()
+	{
+		super.showDetails();
+		System.out.println("Interest Rate: "+interestRate);
+	}
+}
