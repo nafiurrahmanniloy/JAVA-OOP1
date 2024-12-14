7017537aba338143784548fe6510dda31371f0c5
Nafiur Rahman <nafiurrahman03112001@gmail.com>
2024-12-14 15:32:31 +0600

    Delete OOP codes from nazmul sir/Nazmul directory

--- OOP codes from nazmul sir/Nazmul/OOP1_C1_Summer_2021-22 (1)/OOP1_C1_Summer_2021-22/1. Data Type/MyClass2.java
+++
@@ -1,6 +0,0 @@
-public class MyClass2 {
-  public static void main(String[] args) {
-    byte myNum = 100;
-    System.out.println(myNum);  
-  }
-}
--- OOP codes from nazmul sir/Nazmul/OOP1_C1_Summer_2021-22 (1)/OOP1_C1_Summer_2021-22/1. Data Type/MyClass3.java
+++
@@ -1,8 +0,0 @@
-class MyClass3 { 
-    public static void main(String args[]) 
-    { 
-        boolean b = true; 
-        if (b == true) 
-            System.out.println("Computer Science"); 
-    } 
-}
